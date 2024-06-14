7b158fc688014e82a8290cce93372425674b5f57
Jason Norwood-Young <jason@10layer.com>
2024-06-14 12:59:49 +0200

    API coding challenge

---
+++ wk1-http-server/src/api.js
@@ -0,0 +1,14 @@
+/*
+When I GET /api/company, I get a list of companies
+When I GET /api/company/:id, I get a single company
+When I POST /api/company, I create a new company
+When I PUT /api/company/:id, I update a company
+When I DELETE /api/company/:id, I delete a company
+
+Same for users
+When I GET /api/user, I get a list of users AND their associated companies
+When I GET /api/user/:id, I get a single user AND their associated company
+When I POST /api/user, I create a new user
+When I PUT /api/user/:id, I update a user
+When I DELETE /api/user/:id, I delete a user
+*/
